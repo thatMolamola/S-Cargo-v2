de5f4a568a37fbbe10334aa260701d12368b1f30
JoaquinCodesOften <joaquin.ifn@gmail.com>
2024-02-26 12:56:16 -0800

    Minor fix to Character Select screen with choosing characters quickly.

--- Assets/Scripts/Menu/Settings/CharacterSelect.cs
+++ Assets/Scripts/Menu/Settings/CharacterSelect.cs
@@ -12,21 +12,26 @@ public class CharacterSelect : MonoBehaviour
     private bool changed = false;
     private int privSnailChoice;
 
+    private bool choosing = false;
+
 
     public void OnCharacterChanged (int snailChosen) {
-        if (snailChosen != privSnailChoice) {
-            StartCoroutine(SnailSwitch());
-            if (changed) {
-                Door.SetBool("charChange", false);
-                SnailContainer.SetBool("charChange", false);
-                changed = false;
-            }
-            privSnailChoice = snailChosen;
-            GlobalControl.Instance.snailChoice = snailChosen;
-            Door.SetBool("charChange", true);
-            SnailContainer.SetBool("charChange", true);
-            if (playerAnim.GetBool("RollUp")) {
-                playerAnim.SetBool("ImRoll", true);
+        if (!choosing) {
+            choosing = true;
+            if (snailChosen != privSnailChoice) {
+                StartCoroutine(SnailSwitch());
+                if (changed) {
+                    Door.SetBool("charChange", false);
+                    SnailContainer.SetBool("charChange", false);
+                    changed = false;
+                }
+                privSnailChoice = snailChosen;
+                GlobalControl.Instance.snailChoice = snailChosen;
+                Door.SetBool("charChange", true);
+                SnailContainer.SetBool("charChange", true);
+                if (playerAnim.GetBool("RollUp")) {
+                    playerAnim.SetBool("ImRoll", true);
+                }
             }
         }
     }
@@ -45,6 +50,7 @@ public class CharacterSelect : MonoBehaviour
         Door.SetBool("charChange", false);
         playerAnim.SetBool("RollUp", false);
         playerAnim.SetBool("ImRoll", false);
+        choosing = false;
     }
 
     //a fun little bonus script to allow the user to click the snail on the charactee select screen
